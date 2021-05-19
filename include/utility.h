21d129b5615fa2d113fd31964ce020e22eedb0e7
jadon <16850875+wolffshots@users.noreply.github.com>
2021-05-19 18:09:12 +0200

    initial commit

---
+++ include/utility.h
@@ -0,0 +1,11 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+void print_chip_info(void);
+
+#endif
---
+++ utility.c
@@ -0,0 +1,17 @@
+#include "utility.h"
+
+void print_chip_info()
+{
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU cores, WiFi%s%s, ",
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    printf("silicon revision %d, ", chip_info.revision);
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    printf("Free heap: %d\n", esp_get_free_heap_size());
+}
